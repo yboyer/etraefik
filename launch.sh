fa2bea9db854ed15d424f7f6da76ea96de25afd3
yboyer <yboyer@w-inn.eu>
2018-01-18 20:37:38 +0100

    Fix Etcd init

--- launch.sh
+++ launch.sh
@@ -1,22 +1,32 @@
 #!/bin/sh -e
 
-cp /etc/traefik/traefik.toml /traefik.toml
+destTraefikConf=/etc/traefik/traefik.toml
+mkdir `dirname $destTraefikConf`
+cp /traefik.toml $destTraefikConf
+insec=' --insecure-transport=false --insecure-skip-tls-verify'
 
+etcd &
 if [ -z $ETCD_USER ] || [ -z $ETCD_PASS ]; then
   echo -e '\e[93m'
   echo 'Warning:'
   echo 'Etcd does not have auth specified by $ETCD_USER and $ETCD_PASS. Skiping.'
   echo -e '\e[0m'
 else
-  etcd &
-  insec=' --insecure-transport=false --insecure-skip-tls-verify'
   etcdctl $insec user add $ETCD_USER:$ETCD_PASS
   etcdctl $insec auth enable
-  kill %1
 
-  sed -i -e 's|# username = "ETCD_USER"|username = "'$ETCD_USER'"|' /traefik.toml
-  sed -i -e 's|# password = "ETCD_PASS"|password = "'$ETCD_PASS'"|' /traefik.toml
+  userOption="--user=$ETCD_USER:$ETCD_PASS"
+
+  sed -i -e 's|# username = "ETCD_USER"|username = "'$ETCD_USER'"|' $destTraefikConf
+  sed -i -e 's|# password = "ETCD_PASS"|password = "'$ETCD_PASS'"|' $destTraefikConf
+
+  echo 'Etcd config done'
 fi
+etcdctl $insec put "/traefik/acme/account/lock" "" $userOption
+etcdctl $insec put "/traefik/acme/account/object" "" $userOption
+kill %1
+
+
 
 if [ -z $TRAEFIK_AUTH ]; then
   echo -e '\e[93m'
@@ -24,8 +34,8 @@ if [ -z $TRAEFIK_AUTH ]; then
   echo 'Traefik does not have basic auth specified by $TRAEFIK_AUTH. Skiping.'
   echo -e '\e[0m'
 else
-  sed -i -e 's|# \[web.auth.basic\]|\[web.auth.basic\]|' /traefik.toml
-  sed -i -e 's|# users = \["TRAEFIK_AUTH"\]|users = \["'$TRAEFIK_AUTH'"\]|' /traefik.toml
+  sed -i -e 's|# \[web.auth.basic\]|\[web.auth.basic\]|' $destTraefikConf
+  sed -i -e 's|# users = \["TRAEFIK_AUTH"\]|users = \["'$TRAEFIK_AUTH'"\]|' $destTraefikConf
 fi
 
 if [ -z $ACME_EMAIL ]; then
@@ -34,7 +44,7 @@ if [ -z $ACME_EMAIL ]; then
   echo 'Traefik acme does not have email specified by $ACME_EMAIL. Skiping.'
   echo -e '\e[0m'
 else
-  sed -i -e 's|email = .*|email = "'$ACME_EMAIL'"|' /traefik.toml
+  sed -i -e 's|email = .*|email = "'$ACME_EMAIL'"|' $destTraefikConf
 fi
 
 exec supervisord
